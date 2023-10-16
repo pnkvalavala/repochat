88c40bffb01639ff889a3249a5f273b3ef059d97
Pavan Valavala <karthikvalavala9@gmail.com>
2023-10-16 19:14:52 +0530

    remove .git in the git_url

--- repochat/utils.py
+++ repochat/utils.py
@@ -33,6 +33,8 @@ def url_name(url):
 def clone_repo(git_url, repo_path):
     if os.path.exists(repo_path):
         shutil.rmtree(repo_path)
+    
+    git_url = git_url.replace(".git", "")
 
     command = f'git clone {git_url}.git {repo_path} && rm -rf {repo_path}/.git'
     subprocess.run(command, shell=True)
